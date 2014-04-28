f8cccaa238ec28ea6af364b1e7fd109689b078ab
Мариета Георгиева <marietambvg@gmail.com>
2014-04-28 11:35:15 -0500

    Changes made by Мариета Георгиева (marietambvg@gmail.com).

--- Workout-Manager/scripts/app/countdown.js
+++ Workout-Manager/scripts/app/countdown.js
@@ -1,17 +1,30 @@
 var app = app || {};
 
 document.addEventListener("deviceready", function() {
-    var isCountdownInputDataValid;
     var countdownHoursInputData;
     var countdownMinutesInputData;
     var countdownSecondsInputData;
-    var countdownHours;
-    var countdownMinutes;
-    var countdownSeconds;
+    var countdownHoursCurrentValue;
+    var countdownMinutesCurrentValue;
+    var countdownSecondsCurrentValue;
+    var vmCountdown;
     
     (function(a) { 
         a.countdownApi = {
             init:function(e) {
+                vmCountdown = kendo.observable({
+                                                   isVisibleCountdownInputs:true,
+                                                   isVisibleCountdownStatus:false,
+                                                   isVisibleCountdownStartButton:true,
+                                                   isVisibleCountdownPauseButton:false,
+                                                   isVisibleCountdownContinueButton:false,
+                                                   isVisibleCountdownResetButton:false,
+                                                   isVisibleFinishCountdownStatus:false,
+                                                   countdownSeconds:countdownSecondsCurrentValue,
+                                                   countdownMinutes:countdownMinutesCurrentValue,
+                                                   countdownHours:countdownHoursCurrentValue,
+                                               });
+                kendo.bind("#countdown-timer-view", vmCountdown, kendo.mobile.ui);
             },
             
             getCountdownInputData:function() {
@@ -19,31 +32,104 @@ document.addEventListener("deviceready", function() {
                 countdownHoursInputData = $("#variable-countdown-hours-input").val();
                 countdownMinutesInputData = $("#variable-countdown-minutes-input").val();
                 countdownSecondsInputData = $("#variable-countdown-seconds-input").val();
-                if (countdownHoursInputData == "" || countdownHoursInputData < 0 || isNaN(countdownHoursInputData)) {
-                    alert("The countdown hours has to be greater than ot equal to 0!");
+                if (countdownHoursInputData === "") {
+                    countdownHoursInputData = 0;
+                }
+                if (countdownMinutesInputData === "") {
+                    countdownMinutesInputData = 0;
+                }
+                if (countdownSecondsInputData === "") {
+                    countdownSecondsInputData = 0;
+                }
+                
+                if (countdownHoursInputData < 0 || isNaN(countdownHoursInputData)) {
+                    alert("The countdown hours has to be greater than or equal to 0!");
                     isCountdownInputDataValid = false;
                 }
-                else if (countdownMinutesInputData == "" || countdownMinutesInputData < 0 || isNaN(countdownMinutesInputData)) {
-                    alert("The countdown minutes has to be greater than ot equal to 0!");
+                else if (countdownMinutesInputData < 0 || isNaN(countdownMinutesInputData)) {
+                    alert("The countdown minutes has to be greater than or equal to 0!");
                     isCountdownInputDataValid = false;
                 }
-                else if (countdownSecondsInputData == "" || countdownSecondsInputData < 0 || isNaN(countdownSecondsInputData)) {
-                    alert("The countdown seconds has to be greater than ot equal to 0!");
+                else if (countdownSecondsInputData < 0 || isNaN(countdownSecondsInputData)) {
+                    alert("The countdown seconds has to be greater than or equal to 0!");
                     isCountdownInputDataValid = false;
                 }
                 else {
-                    countdownHours = countdownHoursInputData;
-                    countdownMinutes = countdownMinutesInputData;
-                    countdownSeconds = countdownSecondsInputData;
-                    a.countdownApi.startCountdown();
+                    if (countdownSecondsInputData === 0 && countdownMinutesInputData === 0 && countdownHoursInputData === 0) {
+                        alert("You did not enter any data!");
+                    }
+                    else {
+                        countdownHoursCurrentValue = countdownHoursInputData;
+                        countdownMinutesCurrentValue = countdownMinutesInputData;
+                        countdownSecondsCurrentValue = countdownSecondsInputData;
+                        vmCountdown.set("countdownSeconds", countdownSecondsCurrentValue);
+                        vmCountdown.set("countdownMinutes", countdownMinutesCurrentValue);
+                        vmCountdown.set("countdownHours", countdownHoursCurrentValue);
+                        
+                        a.countdownApi.countdownTimer = setInterval(a.countdownApi.startCountdown, 1000);
+                    }
+                }
+            },
+                                  
+            startCountdown:function() {
+                //alert("Everything is correct");
+                vmCountdown.set("isVisibleCountdownInputs", false);
+                vmCountdown.set("isVisibleFinishCountdownStatus", false);
+                vmCountdown.set("isVisibleCountdownStatus", true);
+                vmCountdown.set("isVisibleCountdownStartButton", false);
+                vmCountdown.set("isVisibleCountdownContinueButton", false);
+                vmCountdown.set("isVisibleCountdownPauseButton", true);
+            
+                if (countdownSecondsCurrentValue > 0) {
+                    countdownSecondsCurrentValue--;
+                    vmCountdown.set("countdownSeconds", countdownSecondsCurrentValue);
+                }
+                else if (countdownMinutesCurrentValue > 0) {
+                    countdownSecondsCurrentValue = 59;
+                    countdownMinutesCurrentValue--;
+                    vmCountdown.set("countdownSeconds", countdownSecondsCurrentValue);
+                    vmCountdown.set("countdownMinutes", countdownMinutesCurrentValue);
+                }
+                else if (countdownHoursCurrentValue > 0) {
+                    countdownSecondsCurrentValue = 59;
+                    countdownMinutesCurrentValue = 59;
+                    countdownHoursCurrentValue--;
+                    vmCountdown.set("countdownSeconds", countdownSecondsCurrentValue);
+                    vmCountdown.set("countdownMinutes", countdownMinutesCurrentValue);
+                    vmCountdown.set("countdownHours", countdownHoursCurrentValue);
+                }
+                else {
+                    a.countdownApi.beep();
                 }
             },
             
-            startCountdown:function() {
-                alert("Everything is correct")
+            pauseCountdown:function() {
+                //alert("Countdown timer is paused! Press O.K. when you are ready to continue.");
+                clearInterval(a.countdownApi.countdownTimer);
+                vmCountdown.set("isVisibleCountdownContinueButton", true);
+                vmCountdown.set("isVisibleCountdownPauseButton", false);
             },
             
-            close: function() { 
+            continueCountdown:function() {
+                vmCountdown.set("isVisibleCountdownContinueButton", false);
+                vmCountdown.set("isVisibleCountdownPauseButton", true);
+                a.countdownApi.countdownTimer = setInterval(a.countdownApi.startCountdown, 1000);
+            },
+            
+            resetCountdown:function() {
+                //alert("You are going to reset countdown timer!");
+                a.countdownApi.init();
+            },
+            
+            beep: function() {
+                clearInterval(a.countdownApi.countdownTimer);
+                navigator.notification.beep(3);
+                vmCountdown.set("isVisibleCountdownResetButton",true);
+                vmCountdown.set("isVisibleCountdownPauseButton",false);
+                vmCountdown.set("isVisibleFinishCountdownStatus", true);
+            },
+            close:function() {
+                clearInterval(a.countdownApi.countdownTimer);
             },
         };
     }(app));
--- Workout-Manager/scripts/app/stopwatch.js
+++ Workout-Manager/scripts/app/stopwatch.js
@@ -1,14 +1,106 @@
 var app = app || {};
 
 document.addEventListener("deviceready", function() {
-   
-    
+    var stopwatchHoursCurrentStatus;
+    var stopwatchMinutesCurrentStatus;
+    var stopwatchSecondsCurrentStatus;
+    var vmStopwatch;
     (function(a) { 
-        a.stopwatch = {
+        a.stopwatchApi = {
             init:function(e) {
+                stopwatchHoursCurrentStatus = 0;
+                stopwatchMinutesCurrentStatus = 0;
+                stopwatchSecondsCurrentStatus = 0;
+                vmStopwatch = kendo.observable({
+                                                   isVisibleStopwatchStartButton:true,
+                                                   isVisibleStopwatchPauseButton:false,
+                                                   isVisibleStopwatchStopButton:false,
+                                                   isVisibleStopwatchContinueButton:false,
+                                                   isVisibleStopwatchResetButton:false,
+                                                   isVisibleStopwatchStatus:true,
+                                                   stopwatchHours:stopwatchHoursCurrentStatus,
+                                                   stopwatchMinutes:stopwatchMinutesCurrentStatus,
+                                                   stopwatchSeconds:stopwatchSecondsCurrentStatus,
+                                               });
+                vmStopwatch.set("isVisibleStopwatchStartButton", true);
+                kendo.bind(e.view.element, vmStopwatch, kendo.mobile.ui);
+                a.stopwatchApi.viewStopwatch();
+            },
+            
+            viewStopwatch:function() {
+                vmStopwatch.set("stopwatchHours", stopwatchHoursCurrentStatus);
+                vmStopwatch.set("stopwatchMinutes", stopwatchMinutesCurrentStatus);
+                vmStopwatch.set("stopwatchSeconds", stopwatchSecondsCurrentStatus);
+                vmStopwatch.set("isVisibleStopwatchStartButton", true);
+                vmStopwatch.set("isVisibleStopwatchPauseButton", false); 
+                vmStopwatch.set("isVisibleStopwatchStopButton", false); 
+                vmStopwatch.set("isVisibleStopwatchStatus", true);
+                vmStopwatch.set("isVisibleStopwatchContinueButton", false);
+                vmStopwatch.set("isVisibleStopwatchResetButton", false);
+            },
+            
+            
+            startStopwatch:function() {
+                vmStopwatch.set("isVisibleStopwatchStartButton", false);
+                vmStopwatch.set("isVisibleStopwatchPauseButton", true); 
+                vmStopwatch.set("isVisibleStopwatchStopButton", true); 
+                vmStopwatch.set("isVisibleStopwatchStatus", true);
+                vmStopwatch.set("isVisibleStopwatchContinueButton", false);
+                a.stopwatchApi.stopwatchTimer = setInterval(a.stopwatchApi.countTimeStopwatch, 1000);
+            },
+            
+            countTimeStopwatch:function() {
+                stopwatchSecondsCurrentStatus++;
+                if(stopwatchSecondsCurrentStatus===60){
+                    stopwatchSecondsCurrentStatus=0;
+                    stopwatchMinutesCurrentStatus++;
+                    if(stopwatchMinutesCurrentStatus===60){
+                        stopwatchMinutesCurrentStatus=0;
+                        stopwatchHoursCurrentStatus++;
+                    }
+                }
+                vmStopwatch.set("stopwatchHours", stopwatchHoursCurrentStatus);
+                vmStopwatch.set("stopwatchMinutes", stopwatchMinutesCurrentStatus);
+                vmStopwatch.set("stopwatchSeconds", stopwatchSecondsCurrentStatus);
+            },
+            
+            pauseStopwatch:function() {
+                vmStopwatch.set("isVisibleStopwatchStartButton", false);
+                vmStopwatch.set("isVisibleStopwatchPauseButton", false); 
+                vmStopwatch.set("isVisibleStopwatchStopButton", true); 
+                vmStopwatch.set("isVisibleStopwatchStatus", true);
+                vmStopwatch.set("isVisibleStopwatchContinueButton", true);
+                clearInterval(a.stopwatchApi.stopwatchTimer);
+            },
+            
+            stopStopwatch:function() {
+                clearInterval(a.stopwatchApi.stopwatchTimer);
+                vmStopwatch.set("isVisibleStopwatchStartButton", false);
+                vmStopwatch.set("isVisibleStopwatchPauseButton", false); 
+                vmStopwatch.set("isVisibleStopwatchStopButton", false); 
+                vmStopwatch.set("isVisibleStopwatchStatus", true);
+                vmStopwatch.set("isVisibleStopwatchContinueButton", false);
+                vmStopwatch.set("isVisibleStopwatchResetButton", true);
+            },
+            
+            resetStopwatch:function() {
+                stopwatchHoursCurrentStatus = 0;
+                stopwatchMinutesCurrentStatus = 0;
+                stopwatchSecondsCurrentStatus = 0;
+                a.stopwatchApi.viewStopwatch();
+            },
+            
+            continueStopwatch:function() {
+                vmStopwatch.set("isVisibleStopwatchStartButton", false);
+                vmStopwatch.set("isVisibleStopwatchPauseButton", true); 
+                vmStopwatch.set("isVisibleStopwatchStopButton", true); 
+                vmStopwatch.set("isVisibleStopwatchStatus", true);
+                vmStopwatch.set("isVisibleStopwatchContinueButton", false);
+                a.stopwatchApi.stopwatchTimer = setInterval(a.stopwatchApi.countTimeStopwatch, 1000);
             },
             
             close: function() { 
+                clearInterval(a.stopwatchApi.stopwatchTimer);
             },
         };
     }(app));
